9329cbc7cb962808e8bfafb277dc9497ef4e9f52
Victor Guerra Veloso <victorgvbh@gmail.com>
2020-09-19 17:07:22 -0300

    Test var usage implemented

--- src/tests/br/ufmg/engsoft/reprova/tests/mime/json/JsonTest.java
+++ src/tests/br/ufmg/engsoft/reprova/tests/mime/json/JsonTest.java
@@ -1,7 +1,13 @@
 package br.ufmg.engsoft.reprova.tests.mime.json;
 
+import java.util.AbstractMap;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,34 +18,53 @@ import br.ufmg.engsoft.reprova.model.Semester;
 
 
 public class JsonTest {
+
+    public static <K, V> Map.Entry<K, V> entry(K key, V value) {
+        return new AbstractMap.SimpleEntry<>(key, value);
+    }
+
+    public static <K, U> Collector<Map.Entry<K, U>, ?, Map<K, U>> entriesToMap() {
+        return Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue());
+    }
+
+    public static <K, U> Collector<Map.Entry<K, U>, ?, ConcurrentMap<K, U>> entriesToConcurrentMap() {
+        return Collectors.toConcurrentMap((e) -> e.getKey(), (e) -> e.getValue());
+    }
+
   /**
    * Rendering then parsing should produce an equivalent object.
    */
   @Test
   void question() {
-    var question = new Question.Builder()
+    Question question = new Question.Builder()
       .id("id")
       .theme("theme")
       .description("description")
       .statement("statement")
       .record(
-        Map.of(
-          new Semester(2019, Semester.Reference._1), Map.of(
-            "tw", 50.0f,
-            "tz", 49.5f,
-            "tx", 51.2f
-          ),
-          new Semester(2020, Semester.Reference._2), Collections.emptyMap()
-        )
+              (Map<Semester, Map<String, Float>>)Stream.of(
+                      entry(
+                              new Semester(2019, Semester.Reference._1),
+                              (Map<String, Float>)Stream.of(
+                                      entry("tw", 50.0f),
+                                      entry("tz", 49.5f),
+                                      entry("tx", 51.2f)
+                              ).collect(entriesToMap())
+                      ),
+                      entry(
+                              new Semester(2020, Semester.Reference._2),
+                              Collections.emptyMap()
+                      )
+              ).collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue()))
       )
       .pvt(false)
       .build();
 
-    var formatter = new Json();
+    Json formatter = new Json();
 
-    var json = formatter.render(question);
+    String json = formatter.render(question);
 
-    var questionCopy = formatter
+    Question questionCopy = formatter
       .parse(json, Question.Builder.class)
       .build();
 
