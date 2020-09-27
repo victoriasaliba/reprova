896ef83a8dd43a224d5a6ca1966a4f1591443e54
Daniel Miranda <danielvcmiranda@gmail.com>
2020-09-27 17:21:17 -0300

    setting new variability ScoreFileType

--- src/java/br/ufmg/engsoft/reprova/Configuration.java
+++ src/java/br/ufmg/engsoft/reprova/Configuration.java
@@ -1,6 +1,5 @@
 package br.ufmg.engsoft.reprova;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
@@ -10,33 +9,86 @@ import java.util.Properties;
  */
 public class Configuration {
     public enum Granularity { COARSE_GRAINED, FINE_GRAINED }
+    public enum ScoreFileType {CSV, JSON}
     private static Granularity granularity = null;
+    private static ScoreFileType scoreFileType = null;
     private static boolean isLoaded = false;
-    public static void readGranularity() {
+    public static void readConfiguration() {
         if (!isLoaded) {
             isLoaded = true;
             try {
                 Properties prop = new Properties();
                 InputStream is = ClassLoader.class.getResourceAsStream("/config.properties");
                 prop.load(is);
-                granularity = Granularity.valueOf(prop.getProperty("Granularity"));
+                readGranularity(prop);
+                readScoreFileType(prop);
             } catch (Exception e) {
                 granularity = Granularity.COARSE_GRAINED;
+                scoreFileType = ScoreFileType.CSV;
+
             }
         }
     }
+    public static void readGranularity(Properties properties){
+        try{
+            granularity = Granularity.valueOf(properties.getProperty("Granularity"));
+        }catch (Exception e){
+            granularity = Granularity.COARSE_GRAINED;
+        }
+    }
+    public static void readScoreFileType(Properties properties){
+        try{
+            scoreFileType = ScoreFileType.valueOf(properties.getProperty("ScoreFileType"));
+        }catch (Exception e){
+            scoreFileType = ScoreFileType.CSV;
+        }
+    }
     public static void setCoarseGrained() {
-        isLoaded = true;
+        if (Objects.isNull(scoreFileType)){
+            readConfiguration();
+        }
+        else{
+            isLoaded = true;
+        }
         granularity = Granularity.COARSE_GRAINED;
     }
     public static void setFineGrained() {
-        isLoaded = true;
+        if (Objects.isNull(scoreFileType)){
+            readConfiguration();
+        }
+        else{
+            isLoaded = true;
+        }
         granularity = Granularity.FINE_GRAINED;
     }
     public static boolean isFineGrained() {
         if (Objects.isNull(granularity)) {
-            readGranularity();
+            readConfiguration();
         }
         return granularity.equals(Granularity.FINE_GRAINED);
+    }
+    public static void setScoreFileCsv(){
+        if (Objects.isNull(granularity)){
+            readConfiguration();
+        }
+        else{
+            isLoaded = true;
+        }
+        scoreFileType = ScoreFileType.CSV;
+    }
+    public static void setScoreFileJson(){
+        if (Objects.isNull(granularity)){
+            readConfiguration();
+        }
+        else{
+            isLoaded = true;
+        }
+        scoreFileType = ScoreFileType.JSON;
+    }
+    public static boolean isCsv(){
+        if (Objects.isNull(granularity)) {
+            readConfiguration();
+        }
+        return scoreFileType.equals(ScoreFileType.CSV);
     }
 }
